0a9d939d840c938bb879a9ea743102472012938d
Alan <alan0988469026@gmail.com>
2020-09-22 01:00:15 +0800

    fix(cmp_4bits): fix testbench test case coverage

--- lab1/Comparator_4bits/Lab1_TeamX_Comparator_4bits_t.v
+++ lab1/Comparator_4bits/Lab1_TeamX_Comparator_4bits_t.v
@@ -1,23 +1,24 @@
 `timescale 1ns/1ps
 
 module Comparator_4bits_t;
-reg [4-1:0] a = 4'b0;
-reg [4-1:0] b = 4'b0;
-//reg a = 1'b0;
-//reg b = 1'b0;
-wire lt, gt, eq;
 
-Comparator_4bits cmp(
-    .a(a),
-    .b(b),
-    .a_lt_b(lt),
-    .a_gt_b(gt),
-    .a_eq_b(eq)
-    );
+parameter SIZE = 4;
+
+reg [SIZE-1:0] a = 4'b0;
+reg [SIZE-1:0] b = 4'b0;
+wire lt, eq, gt;
+
+Comparator_4bits cmp_4bits_0(
+  .a(a),
+  .b(b),
+  .a_lt_b(lt),
+  .a_eq_b(eq),
+  .a_gt_b(gt)
+);
     
 initial begin
-  repeat (2 ** 5) begin
-    #1 {a, b} = {a, b} + 1'b1;
+  repeat (2 ** 8) begin
+    #1 {a, b} = {a, b} + 8'b1;
   end
   #1 $finish;
 end
