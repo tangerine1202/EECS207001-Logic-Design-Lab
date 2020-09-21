e312000f41beca1737da87295654b47cf7385385
Alan <alan0988469026@gmail.com>
2020-09-21 20:08:58 +0800

    fix: race condition

--- lab1/Decoder/Lab1_TeamX_Decoder_t.v
+++ lab1/Decoder/Lab1_TeamX_Decoder_t.v
@@ -1,13 +1,12 @@
 `timescale 1ns/1ps
 
-// FIXME: output format is right, but 1 become X (at decoder_3x8);
 module Decoder_t;
 
 parameter SIZE_IN = 4;
 parameter SIZE_OUT = 16;
 
 reg [SIZE_IN-1:0] din = 4'b0;
-wire [SIZE_OUT-1:0] dout = 16'b0;
+wire [SIZE_OUT-1:0] dout;
 
 Decoder decoder_0 (
   .din(din),
@@ -16,7 +15,7 @@ Decoder decoder_0 (
 
 initial begin
   repeat (2 ** 4) begin
-    #1 din = din + 4'b1;   
+    #1 din = din + 4'b1;
   end
   #1 $finish;
 end
