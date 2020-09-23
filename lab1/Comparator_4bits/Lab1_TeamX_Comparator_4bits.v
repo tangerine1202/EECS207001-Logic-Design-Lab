06f0c27348aaa49fec30d9a25d5c04ebcf30e43d
Alan <alan0988469026@gmail.com>
2020-09-23 13:25:40 +0800

    fi: code style

--- lab1/Comparator_4bits/Lab1_TeamX_Comparator_4bits.v
+++ lab1/Comparator_4bits/Lab1_TeamX_Comparator_4bits.v
@@ -1,63 +1,67 @@
 `timescale 1ns/1ps
 
 module Comparator_4bits (a, b, a_lt_b, a_gt_b, a_eq_b);
-input [4-1:0] a, b;
+
+parameter SIZE = 4;
+
+input [SIZE-1:0] a, b;
 output a_lt_b, a_gt_b, a_eq_b;
-wire [4-1:0] eq, gt;
-wire [4-1-1:0] leading_gt;
 
-Eq_1bit eq0 (
-    .a(a[0]),
-    .b(b[0]),
-    .a_eq_b(eq[0])
+wire [SIZE-1:0] eq, gt;
+wire [SIZE-1-1:0] leading_gt;
+
+Eq_1bit eq_0 (
+  .a(a[0]),
+  .b(b[0]),
+  .out(eq[0])
 );
-Eq_1bit eq1 (
-    .a(a[1]),
-    .b(b[1]),
-    .a_eq_b(eq[1])
+Eq_1bit eq_1 (
+  .a(a[1]),
+  .b(b[1]),
+  .out(eq[1])
 );
-Eq_1bit eq2 (
-    .a(a[2]),
-    .b(b[2]),
-    .a_eq_b(eq[2])
+Eq_1bit eq_2 (
+  .a(a[2]),
+  .b(b[2]),
+  .out(eq[2])
 );
-Eq_1bit eq3 (
-    .a(a[3]),
-    .b(b[3]),
-    .a_eq_b(eq[3])
+Eq_1bit eq_3 (
+  .a(a[3]),
+  .b(b[3]),
+  .out(eq[3])
 );
 
-Gt_1bit gt0 (
-    .a(a[0]),
-    .b(b[0]),
-    .a_gt_b(gt[0])
+Gt_1bit gt_0 (
+  .a(a[0]),
+  .b(b[0]),
+  .out(gt[0])
 );
-Gt_1bit gt1 (
-    .a(a[1]),
-    .b(b[1]),
-    .a_gt_b(gt[1])
+Gt_1bit gt_1 (
+  .a(a[1]),
+  .b(b[1]),
+  .out(gt[1])
 );
-Gt_1bit gt2 (
-    .a(a[2]),
-    .b(b[2]),
-    .a_gt_b(gt[2])
+Gt_1bit gt_2 (
+  .a(a[2]),
+  .b(b[2]),
+  .out(gt[2])
 );
-Gt_1bit gt3 (
-    .a(a[3]),
-    .b(b[3]),
-    .a_gt_b(gt[3])
+Gt_1bit gt_3 (
+  .a(a[3]),
+  .b(b[3]),
+  .out(gt[3])
 );
 
 // eq
-and and_eq (a_eq_b, eq[0], eq[1], eq[2], eq[3]);
+and and_eq_0 (a_eq_b, eq[0], eq[1], eq[2], eq[3]);
 
 // gt
-and and2 (leading_gt[2], eq[3], gt[2]);
-and and1 (leading_gt[1], eq[3], eq[2], gt[1]);
-and and0 (leading_gt[0], eq[3], eq[2], eq[1], gt[0]);
-or or0 (a_gt_b, gt[3], leading_gt[2], leading_gt[1], leading_gt[0]);
+and and_gt_2 (leading_gt[2], eq[3], gt[2]);
+and and_gt_1 (leading_gt[1], eq[3], eq[2], gt[1]);
+and and_gt_0 (leading_gt[0], eq[3], eq[2], eq[1], gt[0]);
+or  or_gt_0  (a_gt_b, gt[3], leading_gt[2], leading_gt[1], leading_gt[0]);
 
 // lt
-nor nor0 (a_lt_b, a_eq_b, a_gt_b);
+nor nor_lt_0 (a_lt_b, a_eq_b, a_gt_b);
 
 endmodule
--- lib/MUX/Mux_1bit.v
+++ lib/MUX/Mux_1bit.v
@@ -1,15 +1,16 @@
 `timescale 1ns/1ps
 
 module Mux_1bit (out, in0, in1, sel);
+
 input in0, in1;
 input sel;
 output out;
 
-wire sel_n, and_in0, and_in1;
+wire sel_n, and_in_0, and_in_1;
 
 not not0 (sel_n, sel);
-and and0 (and_in1, in1, sel);
-and and1 (and_in0, in0, sel_n);
-or  or0  (out, and_in1, and_in0);
+and and0 (and_in_1, in1, sel);
+and and1 (and_in_0, in0, sel_n);
+or  or0  (out, and_in_1, and_in_0);
 
 endmodule
