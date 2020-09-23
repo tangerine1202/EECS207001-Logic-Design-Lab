ce56a0e234b0696024e095e156ada7fa0cfda0d6
Alan <alan0988469026@gmail.com>
2020-09-23 20:59:44 +0800

    fix: use multiple module declare syntax

--- lib/MUX/Mux_4bits.v
+++ lib/MUX/Mux_4bits.v
@@ -9,9 +9,6 @@ input [SIZE-1:0] in1;
 input sel;
 output [SIZE-1:0] out;
 
-Mux_1bit mux_1bit_0 (out[0], in1[0], in0[0], sel);
-Mux_1bit mux_1bit_1 (out[1], in1[1], in0[1], sel);
-Mux_1bit mux_1bit_2 (out[2], in1[2], in0[2], sel);
-Mux_1bit mux_1bit_3 (out[3], in1[3], in0[3], sel);
+Mux_1bit mux_1bit [SIZE-1:0] (out, in1, in0, sel);
 
 endmodule
--- lib/MUX/Mux_8bits.v
+++ lib/MUX/Mux_8bits.v
@@ -9,13 +9,6 @@ input [SIZE-1:0] in1;
 input sel;
 output [SIZE-1:0] out;
 
-Mux_1bit mux_1bit_0 (out[0], in1[0], in0[0], sel);
-Mux_1bit mux_1bit_1 (out[1], in1[1], in0[1], sel);
-Mux_1bit mux_1bit_2 (out[2], in1[2], in0[2], sel);
-Mux_1bit mux_1bit_3 (out[3], in1[3], in0[3], sel);
-Mux_1bit mux_1bit_4 (out[4], in1[4], in0[4], sel);
-Mux_1bit mux_1bit_5 (out[5], in1[5], in0[5], sel);
-Mux_1bit mux_1bit_6 (out[6], in1[6], in0[6], sel);
-Mux_1bit mux_1bit_7 (out[7], in1[7], in0[7], sel);
+Mux_1bit mux_1bit [SIZE-1:0] (out, in1, in0, sel);
 
 endmodule
