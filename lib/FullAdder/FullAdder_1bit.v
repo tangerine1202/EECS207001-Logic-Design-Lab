1ae343cabc99a349be17e3c1e7d6b7f04de7ac4b
Ice1187 <Icing1187@gmail.com>
2020-09-23 21:29:28 -0700

    fix: fix mux sel in FA

--- lib/FullAdder/FullAdder_1bit.v
+++ lib/FullAdder/FullAdder_1bit.v
@@ -22,15 +22,9 @@ Nxor_1bit nxor_1bit_1 (
   .out(sum)
 );
 
-<<<<<<< HEAD
 Mux_1bit m1 (
   .in1(a),
   .in0(cin),
-=======
-Mux_1bit mux_1bit_0 (
-  .in1(cin),
-  .in0(a),
->>>>>>> 584f5812fe30a67f3853ad0ec044f118be6ec747
   .sel(x1),
   .out(cout)
 );
