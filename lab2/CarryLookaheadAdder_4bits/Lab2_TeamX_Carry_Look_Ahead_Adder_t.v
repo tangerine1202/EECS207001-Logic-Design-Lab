a701e66c47e6bdeb54f91c76f1234a6ec047eb3c
Alan <alan0988469026@gmail.com>
2020-10-06 14:37:49 +0800

    feat(lab2): cla-4bit finish

--- lab2/CarryLookaheadAdder_4bits/Lab2_TeamX_Carry_Look_Ahead_Adder_t.v
+++ lab2/CarryLookaheadAdder_4bits/Lab2_TeamX_Carry_Look_Ahead_Adder_t.v
@@ -2,4 +2,54 @@
 
 module Carry_Look_Ahead_Adder_t;
 
+parameter SIZE = 4;
+
+reg [SIZE-1:0] a = 4'b0;
+reg [SIZE-1:0] b = 4'b0;
+reg cin = 1'b0;
+wire cout;
+wire [SIZE-1:0] sum;
+
+integer t_a = 0;
+integer t_b = 0;
+integer t_cin = 0;
+reg [3:0] t_sum = 0;
+reg t_cout = 0;
+
+Carry_Look_Ahead_Adder cla (
+  .a (a),
+  .b (b),
+  .cin (cin),
+  .cout (cout),
+  .sum (sum)
+);
+
+
+initial begin
+  repeat (2 ** 8) begin
+    #1
+    {a, b} = {a, b} + 8'b1;
+    assertion(a, b, cin, cout, sum);
+    #1
+    cin = cin + 1'b1;
+    assertion(a, b, cin, cout, sum);
+  end
+  #1 $finish;
+end
+
+task assertion;
+  input [SIZE-1:0] a;
+  input [SIZE-1:0] b;
+  input cin;
+  input cout;
+  input [SIZE-1:0] sum;
+  begin
+    t_a = a;
+    t_b = b;
+    t_cin = cin;
+    t_sum = (t_a + t_b + t_cin);
+    t_cout = (t_a + t_b + t_cin) >> 4;
+  end
+endtask
+
 endmodule
