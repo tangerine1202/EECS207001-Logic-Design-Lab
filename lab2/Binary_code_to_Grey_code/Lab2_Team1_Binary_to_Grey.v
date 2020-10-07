bc2fd850d1700fbca997b005d1e2bedb9550a534
Alan Huang <alanhuang@AlandeMacBook-Air.local>
2020-10-07 10:16:18 +0800

    fix: clear

--- lab2/Binary_code_to_Grey_code/Lab2_Team1_Binary_to_Grey.v
+++ lab2/Binary_code_to_Grey_code/Lab2_Team1_Binary_to_Grey.v
@@ -7,13 +7,13 @@ output [4-1:0] dout;
 
 wire [4-1:0] din_n;
 
-nand nand0 (din_n[0], din[0]);
-nand nand1 (din_n[1], din[1]);
-nand nand2 (din_n[2], din[2]);
-nand nand3 (din_n[3], din[3]);
+nand nand0 (din_n[0], din[0], din[0]);
+nand nand1 (din_n[1], din[1], din[1]);
+nand nand2 (din_n[2], din[2], din[2]);
+nand nand3 (din_n[3], din[3], din[3]);
 
 // output[3] equal to input[3]
-nand nand4 (dout[3], din_n[3]);
+nand nand4 (dout[3], din_n[3], din_n[3]);
 
 // output[2] control by input [3]
 Mux_1bit_in_nand mux0 (
--- lab2/CarryLookaheadAdder_4bits/Lab2_TeamX_Carry_Look_Ahead_Adder.v
+++ lab2/CarryLookaheadAdder_4bits/Lab2_TeamX_Carry_Look_Ahead_Adder.v
@@ -13,8 +13,8 @@ output [SIZE-1:0] sum;
 wire [SIZE-1:0] p;
 wire [SIZE-1:0] g;
 wire [SIZE-1-1:0] c;  // c1, c2, c3
-wire pg;  // not used in lab2
-wire gg;  // not used in lab2
+// wire pg;  // not used in lab2
+// wire gg;  // not used in lab2
 
 
 FullAdder_1bit_in_nand fa0  (
@@ -57,41 +57,41 @@ Carry_Look_Ahead_4bits cla_4bit (
   .p(p),
   .g(g),
   .cin(cin),
-  .pg(pg),   // not used in lab2
-  .gg(gg),   // not used in lab2
+//  .pg(pg),   // not used in lab2
+//  .gg(gg),   // not used in lab2
   .cout({cout, c[3-1:1-1]})
 );
 
 endmodule
 
 
-module Carry_Look_Ahead_4bits (pg, gg, cout, p, g, cin);
+module Carry_Look_Ahead_4bits (cout, p, g, cin);
 
 parameter SIZE = 4;
 
 input [SIZE-1:0] p;       // Propagate output from FullAdder
 input [SIZE-1:0] g;       // Generate output from FullAdder
 input cin;                // initial carry-in c0
-output pg;                // Group Propagate
-output gg;                // Group Generate
 output [SIZE-1:0] cout;   // Carry-out
+// output pg;                // Group Propagate
+// output gg;                // Group Generate
 
 wire [10-1:0] ws;       // lots of wires to calculate for c[1~4]
 wire [SIZE-1:0] g_n;    // !g
-wire pg_n;              // !pg
-wire [SIZE-1-1:0] gp_n; // !(g AND p)
+// wire pg_n;              // !pg
+// wire [SIZE-1-1:0] gp_n; // !(g AND p)
 
 
 nand nand_g_n [SIZE-1:0] (g_n, g, g);
 
 // cout
-// c1 = g0 + (p0 * c0) 
+// c1 = g0 + (p0 * c0)
 //    = !(!g0 * !(p0 * c0))
 nand nand_pc_n0 (ws[0], p[0], cin);
 nand nand_c1 (cout[1-1], g_n[0], ws[0]);
 
-// c2 = g1 + (p1 * c1) = g1 + (g0 * p1) + (c0 * p0 * p1) 
-//    = !(!g1 * !(g0 * p1) * !(c0 * p0 * p1)) 
+// c2 = g1 + (p1 * c1) = g1 + (g0 * p1) + (c0 * p0 * p1)
+//    = !(!g1 * !(g0 * p1) * !(c0 * p0 * p1))
 nand nand_c2_0 (ws[1], g[0], p[1]);
 nand nand_c2_1 (ws[2], cin, p[1], p[0]);
 nand nand_c2 (cout[2-1], g_n[1], ws[1], ws[2]);
@@ -103,7 +103,7 @@ nand nand_c3_1 (ws[4], g[0], p[2], p[1]);
 nand nand_c3_2 (ws[5], cin, p[2], p[1], p[0]);
 nand nand_c3 (cout[3-1], g_n[2], ws[3], ws[4], ws[5]);
 
-// c4 = g3 + (p3 * c3) = g3 + (g2 * p3) + (g1 * p3 * p2) + (g0 * p3 * p2 * p1) + (cin * p3 * p2 * p1 * p0) 
+// c4 = g3 + (p3 * c3) = g3 + (g2 * p3) + (g1 * p3 * p2) + (g0 * p3 * p2 * p1) + (cin * p3 * p2 * p1 * p0)
 //    = !(!g3 * !(g2 * p3) * !(g1 * p3 * p2) * !(g0 * p3 * p2 * p1) * !(cin * p3 * p2 * p1 * p0))
 nand nand_c4_0(ws[6], g[2], p[3]);
 nand nand_c4_1(ws[7], g[1], p[3], p[2]);
@@ -114,15 +114,15 @@ nand nand_c4 (cout[4-1], g_n[3], ws[6], ws[7], ws[8], ws[9]);
 
 // pg = p0 * p1 * p2 * p3
 //    = !(!(p0 * p1 * p2 * p3))
-nand nand_pg_n (pg_n, p[0], p[1], p[2], p[3]);
-nand nand_pg (pg, pg_n, pg_n);
+// nand nand_pg_n (pg_n, p[0], p[1], p[2], p[3]);
+// nand nand_pg (pg, pg_n, pg_n);
 
 // gg = g3 + (g2 * p3) + (g1 * p3 * p2) + (g0 * p3 * p2 * p1)
 //    = !(!g3 * !(g2 * p3) * !(g1 *p3 * p2) * !(g0 * p3 *p2 * p1))
-nand nand_gp_n_2 (gp_n[2], g[2], p[3]);
-nand nand_gp_n_1 (gp_n[1], g[1], p[3], p[2]);
-nand nand_gp_n_0 (gp_n[0], g[0], p[3], p[2], p[1]);
-nand nand_gg (gg, g_n[3], gp_n[2], gp_n[1], gp_n[0]);
+// nand nand_gp_n_2 (gp_n[2], g[2], p[3]);
+// nand nand_gp_n_1 (gp_n[1], g[1], p[3], p[2]);
+// nand nand_gp_n_0 (gp_n[0], g[0], p[3], p[2], p[1]);
+// nand nand_gg (gg, g_n[3], gp_n[2], gp_n[1], gp_n[0]);
 
 
 endmodule
