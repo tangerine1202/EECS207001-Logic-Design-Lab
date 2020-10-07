39027383d54b72ebcc0c938b725aad67b665e91a
Ice1187 <Icing1187@gmail.com>
2020-10-06 20:56:34 -0700

    feat: multiplier done

--- lab2/Multiplier/Lab2_TeamX_Multiplier.v
+++ lab2/Multiplier/Lab2_TeamX_Multiplier.v
@@ -32,23 +32,22 @@ And_1bit_in_nor and_1bit_in_nor_31 (and_a3b[1], a[3], b[1]);
 And_1bit_in_nor and_1bit_in_nor_32 (and_a3b[2], a[3], b[2]);
 And_1bit_in_nor and_1bit_in_nor_33 (and_a3b[3], a[3], b[3]);
 
-// TODO: check whether p[1] connects to sum[0] in 4-bit fa
 FullAdder_4bits_in_nor fa_4bit_in_nor_0 (
-    .sum({p[1], fa_a01[0], fa_a01[1], fa_a01[2]}),
+    .sum({fa_a01[2], fa_a01[1], fa_a01[0], p[1]}),
     .cout(fa_a01[3]),
-    .a({and_a0b[1], and_a0b[2], and_a0b[3], 1'b0}),
+    .a({1'b0, and_a0b[3], and_a0b[2], and_a0b[1]}),
     .b(and_a1b),
     .cin(1'b0)
 );
 FullAdder_4bits_in_nor fa_4bit_in_nor_1 (
-    .sum({p[2], fa_a12[0], fa_a12[1], fa_a12[2]}),
+    .sum({fa_a12[2], fa_a12[1], fa_a12[0], p[2]}),
     .cout(fa_a12[3]),
     .a(fa_a01),
     .b(and_a2b),
     .cin(1'b0)
 );
 FullAdder_4bits_in_nor fa_4bit_in_nor_2 (
-    .sum({p[3], p[4], p[5], p[6]}),
+    .sum({p[6], p[5], p[4], p[3]}),
     .cout(p[7]),
     .a(fa_a12),
     .b(and_a3b),
