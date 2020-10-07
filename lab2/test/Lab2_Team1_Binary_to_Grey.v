45dd05473254b9315ae5fed3496c7d4745c1ffd8
Alan Huang <alan0988469026@gmail.com>
2020-10-07 19:55:37 +0800

    fix: bugs

--- lab2/test/Lab2_Team1_Binary_to_Grey.v
+++ lab2/test/Lab2_Team1_Binary_to_Grey.v
@@ -53,7 +53,7 @@ wire sel_n;
 wire nand_in_0;
 wire nand_in_1;
 
-nand (sel_n, sel);
+nand (sel_n, sel, sel);
 nand nand0 (nand_in_1, in1, sel);
 nand nand1 (nand_in_0, in0, sel_n);
 nand nand2 (out, nand_in_1, nand_in_0);
