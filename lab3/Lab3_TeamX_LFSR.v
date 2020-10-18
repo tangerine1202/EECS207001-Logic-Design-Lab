cd2ed8791ecba00223db29d647bf600cd443a722
Alan Huang <alan0988469026@gmail.com>
2020-10-18 10:14:17 +0800

    fix(lab3): xor of LFSR to be CC

--- lab3/Lab3_TeamX_LFSR.v
+++ lab3/Lab3_TeamX_LFSR.v
@@ -1,18 +1,21 @@
 `timescale 1ns/1ps
 
 module LFSR (clk, rst_n, out);
+
 input clk, rst_n;
 output out;
 
-reg d1_xor_d4;
+wire d1_xor_d4;
 reg [5-1:0] dff;
 
-always @(posedge clk or negedge rst_n) begin
-    if (rst_n == 0) begin
+assign d1_xor_d4 = dff[1] ^ dff[4];
+assign out = dff[4];
+
+always @(posedge clk) begin
+    if (rst_n == 1'b0) begin
         dff <= 5'b11111;
     end
     else begin
-        d1_xor_d4 <= dff[1] ^ dff[4];
         dff[4] <= dff[3];
         dff[3] <= dff[2];
         dff[2] <= dff[1];
@@ -20,7 +23,5 @@ always @(posedge clk or negedge rst_n) begin
         dff[0] <= d1_xor_d4;
     end
 end
-
-assign out = dff[4];
 
 endmodule
