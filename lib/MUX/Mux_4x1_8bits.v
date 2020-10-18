aa1318e8341df1038c45d6a9e39f2e19617daac2
Alan Huang <alan0988469026@gmail.com>
2020-10-18 10:11:44 +0800

    feat(lib: add more mux

---
+++ lib/MUX/Mux_4x1_8bits.v
@@ -0,0 +1,37 @@
+`timescale 1ns/1ps
+
+module Mux_4x1_8bits (out, in, sel);
+
+parameter SIZE_SEL = 2;
+parameter SIZE_M = 4;
+parameter SIZE_N = 8;
+
+input [SIZE_N-1:0] in [SIZE_M-1:0];  // an <SIZE_N>-bit vector with a depth of <SIZE_M>
+input [SIZE_SEL-1:0] sel;
+output [SIZE_N-1:0] out;
+
+wire [SIZE_N-1:0] out0;
+wire [SIZE_N-1:0] out1;
+
+Mux_2x1_8bits mux_2x1_8bits_0 (
+  .in0(in[0]),
+  .in1(in[1]),
+  .sel(sel[0]),
+  .out(out0)
+);
+
+Mux_2x1_8bits mux_2x1_8bits_1 (
+  .in0(in[2]),
+  .in1(in[3]),
+  .sel(sel[0]),
+  .out(out1)
+);
+
+Mux_2x1_8bits mux_2x1_8bits_2 (
+  .in0(out0),
+  .in1(out1),
+  .sel(sel[1]),
+  .out(out)
+);
+
+endmodule
---
+++ lib/MUX/Mux_4x1_8bits_t.v
@@ -0,0 +1,43 @@
+`timescale 1ns/1ps
+
+module Mux_4x1_8bits_t ();
+
+parameter SIZE_SEL = 2;
+parameter SIZE_M = 4;
+parameter SIZE_N = 8;
+
+reg [SIZE_N-1:0] in [SIZE_M-1:0];  // an <SIZE_N>-bit vector with a depth of <SIZE_M>
+reg [SIZE_SEL-1:0] sel = 2'b0;
+wire [SIZE_N-1:0] out;
+
+// utils
+integer i;
+reg [SIZE_N-1:0] counter;
+
+Mux_4x1_8bits mux_4x1_8bits (
+  .in(in),
+  .sel(sel),
+  .out(out)
+);
+
+
+initial begin
+  initialize;
+  #1
+  repeat(SIZE_M) begin
+    #1 sel = sel + 2'b1;
+  end
+  #1 $finish;
+end
+
+
+task initialize begin
+  $display("init start");
+  counter = 4'b0;
+  for(i=0; i<SIZE_M; i=i+1) begin
+    in[i] = counter;
+    counter = counter + 4'b1;
+    $display("in[%0d] = %0d", i, counter);
+  end
+  $display("init end");
+end
