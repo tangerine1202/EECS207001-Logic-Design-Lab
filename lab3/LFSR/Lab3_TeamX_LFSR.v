5f9fc75fc999cb849c55b90d736fc9ef234d0f69
Alan Huang <alan0988469026@gmail.com>
2020-10-18 19:39:53 +0800

    fix(lab3): Q1 remove unwanted f/f

--- lab3/LFSR/Lab3_TeamX_LFSR.v
+++ lab3/LFSR/Lab3_TeamX_LFSR.v
@@ -1,18 +1,20 @@
 `timescale 1ns/1ps
 
 module LFSR (clk, rst_n, out);
+
 input clk, rst_n;
 output out;
 
-reg d1_xor_d4;
+wire d1_xor_d4;
 reg [5-1:0] dff;
 
-always @(posedge clk or negedge rst_n) begin
+assign d1_xor_d4 = dff[1] ^ dff[4];
+
+always @(posedge clk) begin
     if (rst_n == 0) begin
-        dff <= 5'b11111;
+        dff <= 5'B11111;
     end
     else begin
-        d1_xor_d4 <= dff[1] ^ dff[4];
         dff[4] <= dff[3];
         dff[3] <= dff[2];
         dff[2] <= dff[1];
