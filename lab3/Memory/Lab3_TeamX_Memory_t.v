026c81ebdc539089957d7468f924e4db2b223e8a
Alan Huang <alan0988469026@gmail.com>
2020-10-19 12:31:15 +0800

    fix: fixing tb (not finished yet)

--- lab3/Memory/Lab3_TeamX_Memory_t.v
+++ lab3/Memory/Lab3_TeamX_Memory_t.v
@@ -16,6 +16,11 @@ reg [7-1:0] addr = 7'b0;
 reg [WIDTH-1:0] din = 8'b0;
 wire [WIDTH-1:0] dout;
 
+reg [WIDTH-1:0] out;
+reg [DEPTH-1:0] mem [WIDTH-1:0];
+
+integer idx = 0;
+
 Memory mem_0 (
   .clk(clk),
   .ren(ren),
@@ -25,79 +30,128 @@ Memory mem_0 (
   .dout(dout)
 );
 
+
 always #(`CYC/2) clk = ~clk;
 
 initial begin
-  // No Read Write
-  repeat (2 ** 2) begin
+  // Read before Write Test
+  // test mem[0, 1]
+  for (idx = 0; idx < 2; idx = idx+1) begin
     @ (negedge clk) begin
-      Test(
-        .read(0), 
-        .write(0)
-      );
+      GenerateTest(.read(1), .write(0), idx);
+    end
+    @ (posedge clk) begin
+      Test(.read(1), .write(0), idx);
     end
   end
-  // Read Write at the same time
-  repeat (2 ** 2) begin
+  // test mem[n-2, n-1]
+  for (idx = DEPTH-1; idx > DEPTH-1-2; idx = idx-1) begin
     @ (negedge clk) begin
-      Test(
-        .read(1), 
-        .write(1)
-      );
+      GenerateTest(.read(1), .write(0), idx);
     end
-  end
-
-  repeat(2 ** 2) begin
-    // Write
-    repeat(2 ** 3) begin
-      @ (negedge clk) begin
-        Test(
-          .read(0), 
-          .write(1)
-        );
-      end
-    end
-    // Read  
-    repeat(2 ** 3) begin
-      @ (negedge clk) begin
-        Test(
-          .read(1), 
-          .write(0)
-        );
-      end
+    @ (posedge clk) begin
+      Test(.read(1), .write(0), idx);
     end
   end
 
-  // No Read Write
-  repeat (2 ** 2) begin
+  // Read & Write at the same time Test
+  // test mem[0, 1]
+  for (idx = 0; idx < 2; idx = idx+1) begin
     @ (negedge clk) begin
-      Test(
-        .read(0), 
-        .write(0)
-      );
+      GenerateTest(.read(1), .write(1), idx);
+    end
+    @ (posedge clk) begin
+      Test(.read(1), .write(0), idx);
     end
   end
-  // Read Write at the same time
-  repeat (2 ** 2) begin
+  // test mem[n-2, n-1]
+  for (idx = DEPTH-1; idx > DEPTH-1-2; idx = idx-1) begin
     @ (negedge clk) begin
-      Test(
-        .read(1), 
-        .write(1)
-      );
+      GenerateTest(.read(1), .write(1), idx);
+    end
+    @ (posedge clk) begin
+      Test(.read(1), .write(0), idx);
+    end
+  end
+
+
+  // Write before Read Test
+  // test mem[0, 7]
+  for (idx = 0; idx < 8; idx = idx+1) begin
+    @ (negedge clk) begin
+      GenerateTest(.read(0), .write(1), idx);
+    end
+    @ (posedge clk) begin
+      Test(.read(1), .write(0), idx);
+    end
+  end
+  // test mem[n-1-8, n-1]
+  for (idx = DEPTH-1; idx > DEPTH-1-8; idx = idx-1) begin
+    @ (negedge clk) begin
+      GenerateTest(.read(0), .write(1), idx);
+    end
+    @ (posedge clk) begin
+      Test(.read(1), .write(0), idx);
+    end
+  end
+
+  // Stochastic Test
+
+
+
+  // Read before Write Test
+  // test mem[0, 1]
+  for (idx = 0; idx < 2; idx = idx+1) begin
+    @ (negedge clk) begin
+      GenerateTest(.read(1), .write(0), idx);
+    end
+    @ (posedge clk) begin
+      Test(.read(1), .write(0), idx);
+    end
+  end
+  // test mem[n-2, n-1]
+  for (idx = DEPTH-1; idx > DEPTH-1-2; idx = idx-1) begin
+    @ (negedge clk) begin
+      GenerateTest(.read(1), .write(0), idx);
+    end
+    @ (posedge clk) begin
+      Test(.read(1), .write(0), idx);
+    end
+  end
+
+  // Read & Write at the same time Test
+  // test mem[0, 1]
+  for (idx = 0; idx < 2; idx = idx+1) begin
+    @ (negedge clk) begin
+      GenerateTest(.read(1), .write(1), idx);
+    end
+    @ (posedge clk) begin
+      Test(.read(1), .write(0), idx);
+    end
+  end
+  // test mem[n-2, n-1]
+  for (idx = DEPTH-1; idx > DEPTH-1-2; idx = idx-1) begin
+    @ (negedge clk) begin
+      GenerateTest(.read(1), .write(1), idx);
+    end
+    @ (posedge clk) begin
+      Test(.read(1), .write(0), idx);
     end
   end
   $finish;
 end
 
 task Test;
-    input read;
-    input write;
     begin
-      ren = read;
-      wen = write;
-      // FIXME: large range is hard to test, need to figure out a better ways
-      addr = $urandom_range(0, 16-1);
-      din = $urandom_range(0, 256-1);
+      if (ren == 1'b1) begin
+        mem[addr] = din;
+      end
+      else if (wen == 1'b1) begin
+        out = mem[addr];
+      end
+      else begin
+        out = 8'b0;
+      end
     end
 endtask
 
