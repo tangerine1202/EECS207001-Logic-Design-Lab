fa8b9a9bc9822b4af99aa49c2321b8d76e0da36b
Alan Huang <alan0988469026@gmail.com>
2020-10-20 09:25:34 +0800

    feat(lab3): update Q2 tb (untested)

--- lab3/Memory/Lab3_TeamX_Memory_t.v
+++ lab3/Memory/Lab3_TeamX_Memory_t.v
@@ -20,6 +20,7 @@ reg [WIDTH-1:0] out;
 reg [DEPTH-1:0] mem [WIDTH-1:0];
 
 integer idx = 0;
+integer check_addr = 0;
 
 Memory mem_0 (
   .clk(clk),
@@ -41,7 +42,8 @@ initial begin
       GenerateTest(.read(1), .write(0), idx);
     end
     @ (posedge clk) begin
-      Test(.read(1), .write(0), idx);
+      // Test(.read(1), .write(0), idx);
+      Test;
     end
   end
   // test mem[n-2, n-1]
@@ -50,18 +52,21 @@ initial begin
       GenerateTest(.read(1), .write(0), idx);
     end
     @ (posedge clk) begin
-      Test(.read(1), .write(0), idx);
+      // Test(.read(1), .write(0), idx);
+      Test;
     end
   end
 
-  // Read & Write at the same time Test
+
+  // Read & Write at the same time Test (before Write)
   // test mem[0, 1]
   for (idx = 0; idx < 2; idx = idx+1) begin
     @ (negedge clk) begin
       GenerateTest(.read(1), .write(1), idx);
     end
     @ (posedge clk) begin
-      Test(.read(1), .write(0), idx);
+      // Test(.read(1), .write(0), idx);
+      Test;
     end
   end
   // test mem[n-2, n-1]
@@ -70,89 +75,143 @@ initial begin
       GenerateTest(.read(1), .write(1), idx);
     end
     @ (posedge clk) begin
-      Test(.read(1), .write(0), idx);
+      // Test(.read(1), .write(0), idx);
+      Test;
     end
   end
 
 
-  // Write before Read Test
-  // test mem[0, 7]
+  // Read after Write Test
+  // write mem[0, 7]
   for (idx = 0; idx < 8; idx = idx+1) begin
     @ (negedge clk) begin
       GenerateTest(.read(0), .write(1), idx);
     end
     @ (posedge clk) begin
-      Test(.read(1), .write(0), idx);
+      // Test(.read(1), .write(0), idx);
+      Test;
     end
   end
-  // test mem[n-1-8, n-1]
+  // write mem[n-1-8, n-1]
   for (idx = DEPTH-1; idx > DEPTH-1-8; idx = idx-1) begin
     @ (negedge clk) begin
       GenerateTest(.read(0), .write(1), idx);
     end
     @ (posedge clk) begin
+      // Test(.read(1), .write(0), idx);
+      Test;
+    end
+  end
+  // read mem[0, 7]
+  for (idx = 0; idx < 8; idx = idx+1) begin
+    @ (negedge clk) begin
+      GenerateTest(.read(1), .write(0), idx);
+    end
+    @ (posedge clk) begin
+      // Test(.read(1), .write(0), idx);
+      Test;
+    end
+  end
+  // read mem[n-1-8, n-1]
+  for (idx = DEPTH-1; idx > DEPTH-1-8; idx = idx-1) begin
+    @ (negedge clk) begin
+      GenerateTest(.read(1), .write(0), idx);
+    end
+    @ (posedge clk) begin
+      // Test(.read(1), .write(0), idx);
+      Test;
+    end
+  end
+
+
+  // Read & Write at the same time Test (after Write)
+  // test mem[0, 1]
+  for (idx = 0; idx < 2; idx = idx+1) begin
+    @ (negedge clk) begin
+      GenerateTest(.read(1), .write(1), idx);
+    end
+    @ (posedge clk) begin
       Test(.read(1), .write(0), idx);
     end
   end
+  // test mem[n-2, n-1]
+  for (idx = DEPTH-1; idx > DEPTH-1-2; idx = idx-1) begin
+    @ (negedge clk) begin
+      GenerateTest(.read(1), .write(1), idx);
+    end
+    @ (posedge clk) begin
+      Test(.read(1), .write(0), idx);
+    end
+  end
+
 
   // Stochastic Test
-
-
-
-  // Read before Write Test
-  // test mem[0, 1]
-  for (idx = 0; idx < 2; idx = idx+1) begin
+  repeat (2 ** 6) begin
     @ (negedge clk) begin
-      GenerateTest(.read(1), .write(0), idx);
+      GenerateTest(.read(0), .write(1), -1);
     end
     @ (posedge clk) begin
-      Test(.read(1), .write(0), idx);
+      Test;
     end
-  end
-  // test mem[n-2, n-1]
-  for (idx = DEPTH-1; idx > DEPTH-1-2; idx = idx-1) begin
     @ (negedge clk) begin
-      GenerateTest(.read(1), .write(0), idx);
+      GenerateTest(.read(1), .write(0), -2);
     end
     @ (posedge clk) begin
-      Test(.read(1), .write(0), idx);
+      Test;
     end
   end
 
-  // Read & Write at the same time Test
-  // test mem[0, 1]
-  for (idx = 0; idx < 2; idx = idx+1) begin
-    @ (negedge clk) begin
-      GenerateTest(.read(1), .write(1), idx);
-    end
-    @ (posedge clk) begin
-      Test(.read(1), .write(0), idx);
-    end
-  end
-  // test mem[n-2, n-1]
-  for (idx = DEPTH-1; idx > DEPTH-1-2; idx = idx-1) begin
-    @ (negedge clk) begin
-      GenerateTest(.read(1), .write(1), idx);
-    end
-    @ (posedge clk) begin
-      Test(.read(1), .write(0), idx);
-    end
-  end
   $finish;
 end
 
+
 task Test;
-    begin
-      if (ren == 1'b1) begin
-        mem[addr] = din;
-      end
-      else if (wen == 1'b1) begin
-        out = mem[addr];
-      end
-      else begin
-        out = 8'b0;
-      end
+  begin
+    if (dout !== out) begin
+      $display("[ERROR]");
+      $write("ren: %d\n", ren);
+      $write("wen: %d\n", wen);
+      $write("din: %d\n", din);
+      $write("addr: %d\n", addr);
+      $write("dout: %d\n", dout);
+      $write("out : %d\n", out);
+      $display;
     end
+  end
+endtask
+
+
+task GenerateTest;
+  begin
+    input read;
+    input write;
+    input idx;
+    // generate
+    ren = read;
+    wen = write;
+    din = $urandom_range(0, 256-1);
+    if (idx === -1) begin
+      addr = $uradmon_range(0, 128-1);
+    end
+    else if (idx === -2) begin
+      addr = addr;
+    end
+    else begin
+      addr = idx;
+    end
+
+    // udpate
+    if (ren == 1'b1) begin
+      out = mem[addr];
+    end
+    else if (wen == 1'b1) begin
+      mem[addr] = din;
+      out = 8'b0;
+    end
+    else begin
+      out = 8'b0;
+    end
+  end
 endtask
 
 endmodule
