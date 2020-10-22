ef274404a73a2e7b6e334a2001aa6b8977071c7a
Alan Huang <alan0988469026@gmail.com>
2020-10-22 14:46:01 +0800

    fix(lab3): Q3 logic

--- lab3/Parameterized_Ping_Pong_Counter/Lab3_TeamX_Parameterized_Ping_Pong_Counter.v
+++ lab3/Parameterized_Ping_Pong_Counter/Lab3_TeamX_Parameterized_Ping_Pong_Counter.v
@@ -14,34 +14,48 @@ reg [4-1:0] next_out;
 
 // Sequential: direction
 always @(posedge clk) begin
-    if (rst_n == 1'b0)
+    if (rst_n == 1'b0) begin
         direction <= 1'b1;
-    else
+    end
+    else begin
         direction <= next_direction;
+    end
 end
 
 // Sequential: out
 always @(posedge clk) begin
-    if (rst_n == 1'b0)
+    if (rst_n == 1'b0) begin
         out <= min;
-    else 
+    end
+    else begin
         out <= next_out;
+    end
 end
 
 // Combinational: next_direction
 always @(*) begin
-    if (enable) begin 
+    if (enable) begin
         if (flip == 1'b1) begin
             next_direction = !direction;
-        end
-        else begin 
-            if (out == min) begin
-                next_direction = 1'b1;
-            end
-            else if (out == max) begin
-                next_direction = 1'b0;
-            end
-            else begin 
+        end 
+        else begin
+            if (max > min) begin
+                if (out >= min && out <= max) begin
+                    if (out == min) begin
+                        next_direction = 1'b1;
+                    end
+                    else if (out == max) begin
+                        next_direction = 1'b0;
+                    end
+                    else begin
+                        next_direction = direction;
+                    end
+                end 
+                else begin
+                    next_direction = direction;
+                end
+            end 
+            else begin
                 next_direction = direction;
             end
         end
@@ -53,14 +67,24 @@ end
 
 // Combinational: next_out
 always @(*) begin
-    if (enable && (max > min)) begin
-        if (next_direction == 1'b1 && out < max) begin
-            next_out = out + 1'b1;
+    if (enable) begin
+        if (max > min) begin
+            if (out >= min && out <= max) begin
+                if (next_direction == 1'b1) begin
+                    next_out = out + 4'b0001;
+                end
+                else if (next_direction == 1'b0) begin
+                    next_out = out - 4'b0001;
+                end
+                else begin
+                    next_out = out;
+                end
+            end
+            else begin
+                next_out = out;
+            end
         end
-        else if (next_direction == 1'b0 && out > min) begin
-            next_out = out - 1'b1;
-        end
-        else begin 
+        else begin
             next_out = out;
         end
     end 
@@ -68,5 +92,4 @@ always @(*) begin
         next_out = out;
     end
 end
-
 endmodule
