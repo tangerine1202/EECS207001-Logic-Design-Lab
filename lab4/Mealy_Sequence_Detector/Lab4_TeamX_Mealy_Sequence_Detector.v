8e02dc73974228465d60df1d5887ec5a059b0eb9
Alan Huang <alan0988469026@gmail.com>
2020-11-07 12:01:29 +0800

    feat(lab4): finish Q1

--- lab4/Mealy_Sequence_Detector/Lab4_TeamX_Mealy_Sequence_Detector.v
+++ lab4/Mealy_Sequence_Detector/Lab4_TeamX_Mealy_Sequence_Detector.v
@@ -2,20 +2,22 @@
 
 module Mealy_Sequence_Detector (clk, rst_n, in, dec);
 
-parameter S0 = 3'b000;  // Intial state
-parameter S1 = 3'b001;  // 1st input match state
-parameter S2 = 3'b010;  // 2nd input match state
-parameter S3 = 3'b011;  // 3nd input match state
-parameter S4 = 3'b100;  // 4nd input match state
-parameter F0 = 3'b111;  // S1 failed to match next pattern
-parameter F1 = 3'b110;  // S2 failed to match next pattern
+parameter S0 = 4'h0;  // Intial state
+parameter S1 = 4'h1;  
+parameter S2 = 4'h2;  
+parameter S3 = 4'h3;  
+parameter S5 = 4'h5;  
+parameter S6 = 4'h6; 
+parameter S7 = 4'h7;
+parameter F0 = 4'ha;
+parameter F1 = 4'hb;
 
 input clk, rst_n;
 input in;
 output reg dec;
 
-reg [3-1:0] state;
-reg [3-1:0] next_state;
+reg [4-1:0] state;
+reg [4-1:0] next_state;
 
 
 // SC: state transition
@@ -33,36 +35,44 @@ end
 always @(*) begin
   case (state)
     S0: begin
-      next_state = (in == 1'b0) ? S1 : S1;
-      dec = (in == 1'b0) ? 1'b0 : 1'b0;
+      next_state = (in) ? S1 : S5;
+      dec = (in) ? 1'b0 : 1'b0;
     end
     S1: begin
-      next_state = (in == 1'b0) ? F0 : S2;
-      dec = (in == 1'b0) ? 1'b0 : 1'b0;
+      next_state = (in) ? F0 : S2;
+      dec = (in) ? 1'b0 : 1'b0;
     end
     S2: begin
-      next_state = (in == 1'b0) ? S3 : F1;
-      dec = (in == 1'b0) ? 1'b0 : 1'b0;
+      next_state = (in) ? S3 : F1;
+      dec = (in) ? 1'b0 : 1'b0;
     end
     S3: begin
-      next_state = (in == 1'b0) ? S4 : S4;
-      dec = (in == 1'b0) ? 1'b1 : 1'b1;
+      next_state = (in) ? S0 : S0;
+      dec = (in) ? 1'b1 : 1'b1;
     end
-    S4: begin
-      next_state = (in == 1'b0) ? S0 : S0;
-      dec = (in == 1'b0) ? 1'b0 : 1'b0;
+    S5: begin
+      next_state = (in) ? F0 : S6;
+      dec = (in) ? 1'b0 : 1'b0;
+    end
+    S6: begin
+      next_state = (in) ? S7 : F1;
+      dec = (in) ? 1'b0 : 1'b0;
+    end
+    S7: begin
+      next_state = (in) ? S0 : S0;
+      dec = (in) ? 1'b1 : 1'b0;
     end
     F0: begin
-      next_state = (in == 1'b0) ? F1 : F1;
-      dec = (in == 1'b0) ? 1'b0 : 1'b0;
+      next_state = (in) ? F1 : F1;
+      dec = (in) ? 1'b0 : 1'b0;
     end
     F1: begin
-      next_state = (in == 1'b0) ? S4 : S4;
-      dec = (in == 1'b0) ? 1'b0 : 1'b0;
+      next_state = (in) ? S0 : S0;
+      dec = (in) ? 1'b0 : 1'b0;
     end
     default: begin
-      next_state = (in == 1'b0) ? S0 : S0;
-      dec = (in == 1'b0) ? 1'b0 : 1'b0;
+      next_state = (in) ? S0 : S0;
+      dec = (in) ? 1'b0 : 1'b0;
     end
   endcase
 end
--- lab4/Mealy_Sequence_Detector/Lab4_TeamX_Mealy_Sequence_Detector_t.v
+++ lab4/Mealy_Sequence_Detector/Lab4_TeamX_Mealy_Sequence_Detector_t.v
@@ -3,11 +3,6 @@
 
 module Mealy_Sequence_Detector_t ();
 
-parameter S0 = 3'b000;  // Intial state
-parameter S1 = 3'b001;  // 1st input match state
-parameter S2 = 3'b010;  // 2nd input match state
-parameter S3 = 3'b011;  // 2nd input match state
-parameter F0 = 3'b100;  // State that failed to match the pattern
 
 // IO port
 reg clk = 1'b0;
@@ -30,24 +25,24 @@ reg [2-1:0] i;
 initial begin
   seq = 4'b0000;
   rst_n = 1'b1;
+  #`CYC  rst_n = 1'b0;
+  #`CYC  rst_n = 1'b1;
 
   repeat (2 ** 4) begin
     i = 2'b11;
     in = seq[i];
-    #`CYC  rst_n = 1'b0;
-    #`CYC  rst_n = 1'b1;
-    $display("seq: %b", seq);
+    // $display("seq: %b", seq);
     repeat (4) begin
       @ (posedge clk) begin
+        if (i == 2'b0) Test;
       end
       @ (negedge clk) begin
-        $display("in[%d]: %b", i, in);
-        $display("dec: %b", dec);
+        // $display("in[%d]: %b", i, in);
+        // $display("dec: %b", dec);
         i = i - 2'b1;
         in = seq[i];
       end
     end
-    Test;
     seq = seq + 4'b1;
   end
 
@@ -57,9 +52,9 @@ end
 
 task Test;
 begin
-  if (seq == 4'b1101 || seq == 4'b0011 || seq == 4'b1010) begin
+  if (seq == 4'b1011 || seq == 4'b0011 || seq == 4'b1010) begin
     if (dec != 1'b1) begin
-      $display("Error, should be 0");
+      $display("[ERROR], 'dec' should be 1");
       $write("seq: %b\n", seq);
       $write("dec: %d\n", dec);
       $display;
@@ -67,7 +62,7 @@ begin
   end
   else begin
     if (dec != 1'b0) begin
-      $display("Error, should be 0");
+      $display("[ERROR], 'dec' should be 0");
       $write("seq: %b\n", seq);
       $write("dec: %d\n", dec);
       $display;
