51bdfbdc9d777f6e30945f026b35b13237ee4b0e
Alan Huang <alan0988469026@gmail.com>
2020-11-12 19:46:47 +0800

    chore: use py3 fstring

--- cmd/cad_autotest.py
+++ cmd/cad_autotest.py
@@ -8,10 +8,10 @@ CAD_IC = 'ic23'
 TIME_SEQ = dt.now().strftime('%m%d%H%M%S')
 
 # User
-user = ''  # Your username here, ex: u108062000
-lab = ''  # The prefix of the directory created on CAD, recommend using lab#
-ds = ''  # Design file
-tb = ''  # Testbench file
+user = 'u108062308'  # Your username here, ex: u108062000
+lab = 'lab4'  # The prefix of the directory created on CAD, recommend using lab#
+ds = '../lab4/Mealy_Sequence_Detector/Lab4_TeamX_Mealy_Sequence_Detector.v'  # Design file
+tb = '../lab4/Mealy_Sequence_Detector/Lab4_TeamX_Mealy_Sequence_Detector_t.v'  # Testbench file
 
 # Input
 while user == '':
@@ -26,37 +26,37 @@ while ds == '':
 	if not ds.endswith('.v'):
 		ds += '.v'
 while tb == '' or tb == 'n' or tb == 'no':
-	tb = input('testbench [{}](yes/<path>): '.format(ds[:-2]+'_t.v'))
+	tb = input(f"testbench [{ds[:-2]+'_t.v'}](yes/<path>):")
 	tb.strip()
 	if tb == 'y' or tb == 'yes':
 		tb = ds[:-2] + '_t.v'
 	elif (tb != '') and (not tb.endswith('.v')):
 		tb += '.v'
 
-print("""
-[*] Design:    {}
-[*] Testbench: {}
-""".format(ds, tb))
+print(f"""
+[*] Design:    {ds}
+[*] Testbench: {tb}
+""")
 
 # Remote Command
-user_at_host = '{}@{}'.format(user, CAD_IP)
-dir_name = './autotest/{}/{}_{}/'.format(lab, ds[:-2], TIME_SEQ)
+user_at_host = f'{user}@{CAD_IP}'
+dir_name = f'./autotest/{lab}/{ds[:-2]}{TIME_SEQ}/'
 remote_cmd = ''
 # Setting Cadence License(in order to use ncverilog)
 remote_cmd += 'source /usr/cad/cadence/setup.csh;'
-remote_cmd += 'mkdir -p {};'.format(dir_name)
-remote_cmd += 'mv {} {} {};'.format(ds, tb, dir_name)
-remote_cmd += 'cd {};'.format(dir_name)
-remote_cmd += 'ncverilog {} {};'.format(ds, tb)
+remote_cmd += f'mkdir -p {dir_name};'
+remote_cmd += f'mv {ds} {tb} {dir_name};'
+remote_cmd += f'cd {dir_name};'
+remote_cmd += f'ncverilog {ds} {tb};'
 
-remote_cmd = 'ssh -tt {} \\"{}\\"'.format(CAD_IC, remote_cmd)
+remote_cmd = f'ssh -tt {CAD_IC} \\"{remote_cmd}\\"'
 
 # Connect
 print('[+] Copying file to CAD...')
-os.system('scp {} {} {}:~/'.format(ds, tb, user_at_host))
+os.system(f'scp {ds} {tb} {user_at_host}:~/')
 
 print('[+] Seletct remote host & Run ncverilog...')
-os.system('ssh -tt {} "{}"'.format(user_at_host, remote_cmd))
+os.system(f'ssh -tt {user_at_host} "{remote_cmd}"')
 
 print('[+] Test done.')
 
