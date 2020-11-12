bcdb80277dd92750740fb51b37176d5581c30b11
Ice1187 <Icing1187@gmail.com>
2020-11-12 19:19:25 +0800

    feat(cmd): add lab dir

--- cmd/cad_autotest.py
+++ cmd/cad_autotest.py
@@ -7,27 +7,27 @@ CAD_IP = '140.114.75.200'
 CAD_IC = 'ic23'
 TIME_SEQ = dt.now().strftime('%m%d%H%M%S')
 
-# User 
-user = '' # Your username here, ex: u108062000
-lab  = '' # The prefix of the directory created on CAD, recommend using lab#
-ds   = '' # Design file
-tb   = '' # Testbench file
+# User
+user = ''  # Your username here, ex: u108062000
+lab = ''  # The prefix of the directory created on CAD, recommend using lab#
+ds = ''  # Design file
+tb = ''  # Testbench file
 
 # Input
 while user == '':
-	user = input('user: ')
+    user = input('user: ')
 while lab == '':
-	lab = input('lab name: ')
+    lab = input('lab name: ')
 while ds == '':
-	ds = input('design: ')
-	if not ds.endswith('.v'):
-		ds += '.v'
+    ds = input('design: ')
+    if not ds.endswith('.v'):
+        ds += '.v'
 while tb == '' or tb == 'n' or tb == 'no':
-	tb = input('testbench [{}](yes/<path>): '.format(ds[:-2]+'_t.v'))
-	if tb == 'y' or tb == 'yes':
-		tb = ds[:-2] + '_t.v'
-	elif (tb != '') and (not tb.endswith('.v')):
-		tb += '.v'
+    tb = input('testbench [{}](yes/<path>): '.format(ds[:-2]+'_t.v'))
+    if tb == 'y' or tb == 'yes':
+        tb = ds[:-2] + '_t.v'
+    elif (tb != '') and (not tb.endswith('.v')):
+        tb += '.v'
 
 print("""
 [*] Design:    {}
@@ -36,9 +36,10 @@ print("""
 
 # Remote Command
 user_at_host = '{}@{}'.format(user, CAD_IP)
-dir_name = './autotest/{}_{}/'.format(lab, TIME_SEQ)
-remote_cmd  = ''
-remote_cmd += 'source /usr/cad/cadence/setup.csh;'     # Setting Cadence License(in order to use ncverilog)
+dir_name = './autotest/{}/{}_{}/'.format(lab, ds[:-2], TIME_SEQ)
+remote_cmd = ''
+# Setting Cadence License(in order to use ncverilog)
+remote_cmd += 'source /usr/cad/cadence/setup.csh;'
 remote_cmd += 'mkdir -p {};'.format(dir_name)
 remote_cmd += 'mv {} {} {};'.format(ds, tb, dir_name)
 remote_cmd += 'cd {};'.format(dir_name)
@@ -54,4 +55,3 @@ print('[+] Seletct remote host & Run ncverilog...')
 os.system('ssh -tt {} "{}"'.format(user_at_host, remote_cmd))
 
 print('[+] Test done.')
-
