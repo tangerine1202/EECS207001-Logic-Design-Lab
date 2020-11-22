c6c2a79d7324b21ff2924ac2d2ff8dcd60502caf
Ice1187 <Icing1187@gmail.com>
2020-11-22 16:37:47 +0800

    feat(lab5): Q1 tb add check task

--- lab5/Sliding_Window_Detector/Lab5_TeamX_Sliding_Window_Detector_t.v
+++ lab5/Sliding_Window_Detector/Lab5_TeamX_Sliding_Window_Detector_t.v
@@ -10,6 +10,10 @@ reg in = 1'b0;
 wire dec1;
 wire dec2;
 
+reg [3-1:0] chk1_in;
+reg [4-1:0] chk2_in;
+reg chk1_stop;
+
 Sliding_Window_Detector SWD (
     .clk(clk),
     .rst_n(rst_n),
@@ -28,6 +32,38 @@ initial begin
     dec2_pdf;
 
     $finish;
+end
+
+always @(in) begin
+    // Dec1 check
+    chk1_in[2:1] = chk1_in[1:0]
+    chk1_in[0]   = in;
+    if (chk1_in == 3'b111)
+        chk1_stop = 1'b1;
+    else
+        chk1_stop = 1'b0;
+    if (chk1_stop == 1'b0) begin
+        if (chk1_in == 3'b101) begin
+            check_1(1'b1);
+        end
+        else begin
+            check_1(1'b0);
+        end
+    end
+    else begin
+        check_1(1'b0);
+    end
+
+
+    // Dec2 check
+    chk2_in[3:1] = chk2_in[2:0];
+    chk2_in[0]   = in;
+    if (chk2_in == 4'b1101) begin
+        check_2(1'b1);
+    end
+    else begin
+        check_2(1'b0);
+    end
 end
 
 // testcase on lab5 pdf
@@ -77,11 +113,24 @@ begin
 end
 endtask
 
-task check;
+task check_1(input expect);
 begin
-    
+    if (dec1 != expect) begin
+        $timeformat(-9, 2, " ns", 20);  // specify %t format
+        $display("[-] %t: Dec1 expect %b, but get %b.", $time, expect, dec1);
+    end
 end
 endtask
+
+task check_2(input expect);
+begin
+    if (dec2 != expect) begin
+        $timeformat(-9, 2, " ns", 20);  // specify %t format
+        $display("[-] %t: Dec2 expect %b, but get %b.", $time, expect, dec2);
+    end
+end
+endtask
+
 
 // reset signal
 task reset;
