71d5278e080fce2bfef42ffd7bf357932df31e6f
Ice1187 <Icing1187@gmail.com>
2020-11-23 20:35:50 +0800

    feat(lab5): Q1 done.

--- lab5/Sliding_Window_Detector/Lab5_TeamX_Sliding_Window_Detector_t.v
+++ lab5/Sliding_Window_Detector/Lab5_TeamX_Sliding_Window_Detector_t.v
@@ -1,4 +1,4 @@
-`timescale 1ns/1ps
+`timescale 100ps/1ps
 
 `define CYC 4
 
@@ -10,9 +10,11 @@ reg in = 1'b0;
 wire dec1;
 wire dec2;
 
-reg [3-1:0] chk1_in;
-reg [4-1:0] chk2_in;
-reg chk1_stop;
+reg [3-1:0] chk1_in = 3'b000;
+reg [4-1:0] chk2_in = 3'b000;
+reg chk1_stop = 1'b0;
+
+reg [8-1:0] iter = 8'b0; // iter for tb
 
 Sliding_Window_Detector SWD (
     .clk(clk),
@@ -28,20 +30,37 @@ initial begin
     reset;
     
     dec1_pdf;
-    #(`CYC*2);
+    #(`CYC*2) reset;
     dec2_pdf;
+
+    reset;
+    
+    dec1_ts;
+    #(`CYC*2) reset;
+    dec2_ts;
 
     $finish;
 end
 
-always @(in) begin
+// check in state
+always @(posedge clk) begin
     // Dec1 check
-    chk1_in[2:1] = chk1_in[1:0]
-    chk1_in[0]   = in;
-    if (chk1_in == 3'b111)
+    chk1_in[2:1] <= chk1_in[1:0];
+    chk1_in[0]   <= in;
+    // Dec2 check
+    chk2_in[3:1] = chk2_in[2:0];
+    chk2_in[0]   = in;
+end
+
+// check dec 1
+always @(*) begin
+    if (chk1_in == 3'b111) begin
         chk1_stop = 1'b1;
-    else
-        chk1_stop = 1'b0;
+    end
+    else begin 
+        chk1_stop = chk1_stop;
+    end
+
     if (chk1_stop == 1'b0) begin
         if (chk1_in == 3'b101) begin
             check_1(1'b1);
@@ -53,11 +72,10 @@ always @(in) begin
     else begin
         check_1(1'b0);
     end
+end
 
-
-    // Dec2 check
-    chk2_in[3:1] = chk2_in[2:0];
-    chk2_in[0]   = in;
+// check dec 2
+always @(*) begin
     if (chk2_in == 4'b1101) begin
         check_2(1'b1);
     end
@@ -65,6 +83,42 @@ always @(in) begin
         check_2(1'b0);
     end
 end
+
+// testcase for dec1
+task dec1_ts;
+reg [4-1:0] i;
+begin
+    repeat (2**6) begin
+        iter = iter + 8'b0000_0001;
+        for(i = 4'd0; i < 4'd6; i = i+4'd1) begin
+            if (iter[i] == 1'b0)
+                Zero;
+            else
+                One;
+            #(`CYC*2) reset;  // delay 2 clk to avoid concat situation and reset chk1_stop
+        end
+    end
+    iter = 8'b0000_0000;
+end
+endtask
+
+// testcase for dec2
+task dec2_ts;
+reg [4-1:0] i;
+begin
+    repeat (2**8) begin
+        iter = iter + 8'b0000_0001;
+        for(i = 4'd0; i < 4'd8; i = i+4'd1) begin
+            if (iter[i] == 1'b0)
+                Zero;
+            else
+                One;
+            #(`CYC*2) reset;  // delay 2 clk and reset to avoid concat situation and chk1 error
+        end
+    end
+    iter = 8'b0000_0000;
+end
+endtask
 
 // testcase on lab5 pdf
 task dec1_pdf;
@@ -136,6 +190,8 @@ endtask
 task reset;
 begin
     @(negedge clk) rst_n = 1'b0;
+    in = 1'b0;
+    chk1_stop = 1'b0;
     @(negedge clk) rst_n = 1'b1;
 end
 endtask
