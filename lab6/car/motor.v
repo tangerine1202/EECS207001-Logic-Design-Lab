813524e80cc52124e9daa03cd936e888fef97c3e
Alan Huang <alan0988469026@gmail.com>
2020-12-15 22:43:38 +0800

    feat: pwm add state template

--- lab6/car/motor.v
+++ lab6/car/motor.v
@@ -1,11 +1,21 @@
 module motor(
   input clk,
   input rst,
-  // input [??:0] mode,
+  input [1:0] mode,
   output [1:0] pwm  // {left, right}
 );
 
-  parameter NORMAL_FORWARD = 10'd1000;
+  // mode
+  parameter STOP = 2'b00;
+  parameter GO_STRAIGHT = 2'b01;
+  parameter TURN_LEFT = 2'b10;
+  parameter TURN_RIGHT = 2'b11;
+
+  // duty
+  parameter SPEED_STOP   = 10'd0;
+  parameter SPEED_SLOW   = 10'd800;
+  parameter SPEED_NORMAL = 10'd1000;
+  parameter SPEED_FAST   = 10'd1023;
 
   reg [9:0]next_left_duty, next_right_duty;
   reg [9:0]left_duty, right_duty;
@@ -31,20 +41,36 @@ module motor(
       left_duty <= 10'd0;
       right_duty <= 10'd0;
     end else begin
-      left_duty <= NORMAL_FORWARD;
-      right_duty <= NORMAL_FORWARD;
+      left_duty <= next_left_duty;
+      right_duty <= next_right_duty;
     end
   end
 
   // TODO: take the right speed for different situation
-  // always @(*) begin
-    // // case (mode)
-      // //default: begin
-        // next_left_duty = NORMAL_FORWARD;
-        // next_right_duty = NORMAL_FORWARD;
-      // //end
-    // // endcase
-  // end
+   always @(*) begin
+      case (mode)
+        STOP: begin
+          next_left_duty = SPEED_STOP;
+          next_right_duty = SPEED_STOP;
+        end
+        GO_STRAIGHT: begin
+          next_left_duty = SPEED_NORMAL;
+          next_right_duty = SPEED_NORMAL;
+        end
+        TURN_LEFT: begin
+          next_left_duty = SPEED_SLOW;
+          next_right_duty = SPEED_FAST;
+        end
+        TURN_RIGHT: begin
+          next_left_duty = SPEED_FAST;
+          next_right_duty = SPEED_SLOW;
+        end
+        default: begin
+         next_left_duty = SPEED_NORMAL;
+         next_right_duty = SPEED_NORMAL;
+        end
+      endcase
+   end
 
 endmodule
 
