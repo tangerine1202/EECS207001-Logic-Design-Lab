47b3145d35a821e2f9ebd7a77a799f9ec651ae19
Alan Huang <alan0988469026@gmail.com>
2021-01-17 15:35:45 +0800

    feat: code style

--- final/ArduinoToFPGA/FPGAReceiveFromArduino.v
+++ final/ArduinoToFPGA/FPGAReceiveFromArduino.v
@@ -7,19 +7,26 @@ module Receive_From_Arduino (
 );
 
 //parameter CLK_FREQ  = 32'd100_000_000;
-//parameter UART_BAUD = 32'115200;
+//parameter UART_BAUD = 32'd115_200;
 parameter CLKS_PER_BIT = 32'd868;  // CLK_FREQ / UART_BAUD
 
-parameter IDLE = 2'd0;
-parameter RX_MSB = 2'd1;        // receive MSB state
-parameter RX_LSB = 2'd2;        // receive LSB state
+parameter RX_MSB = 2'd1;        // the state ready to receive MSB
+parameter RX_LSB = 2'd2;        // the state ready to receive LSB
 parameter DATA_READY = 2'd3;
 
-wire rxReady;
-wire [7:0] rxByte;
+wire rxReady;                   // data from uart module is ready
+wire [7:0] rxByte;              // data from uart module
 reg [15:0] nextData;
 reg [1:0] state;
 reg [1:0] nextState;
+
+
+uart_rx #(.CLKS_PER_BIT(CLKS_PER_BIT)) rx_from_uart (
+  .i_Clock(clk),
+  .i_Rx_Serial(serialFromArduino),
+  .o_Rx_DV(rxReady),
+  .o_Rx_Byte(rxByte)
+);
 
 always @(posedge clk) begin
   if (rst == 1'b1) begin
@@ -68,12 +75,5 @@ always @(*) begin
 end
 
 assign isDataReady = (state == DATA_READY) ? 1'b1 : 1'b0;
-
-uart_rx #(.CLKS_PER_BIT(CLKS_PER_BIT)) rx_from_uart (
-  .i_Clock(clk),
-  .i_Rx_Serial(serialFromArduino),
-  .o_Rx_DV(rxReady),
-  .o_Rx_Byte(rxByte)
-);
 
 endmodule
